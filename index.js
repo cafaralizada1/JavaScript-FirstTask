93957aae7a15bbdff83210f06ffe0d376823420f
cafaralizada1 <sd7eq683b@code.edu.az>
2023-10-19 22:57:54 +0300

    JavaScript

---
+++ index.js
@@ -0,0 +1,100 @@
+
+//task1
+
+// let a = 15
+// let b = 10
+
+// if ( a < b ) {
+//     console.log("a bden kicikdir");
+// } else if ( a === b){
+//     console.log("beraberdir");
+// } else {
+//     console.log("a bden boyukdur");
+// }
+
+
+
+
+//task 2 3 4 5 6
+
+// let students=[
+
+//     {id:1, name: "Ali", surname: "Alisoy", age:20, grade: 95},
+    
+//     {id:2, name: "Vusala", surname: "Nabiyeva", age:34, grade: 77},
+    
+//     {id:3, name: "Aliya", surname: "Gurbanzade", age:14, grade: 98},
+    
+//     {id:4, name: "Sabir", surname: "Nuruyev", age:15, grade: 54},
+    
+//     {id:5, name: "Mehman", surname: "Heydarov", age:22, grade: 100},
+    
+//     {id:6, name: "Sevana", surname: "Mammadli", age:41, grade: 35},
+    
+//     {id:7, name: "Ilham", surname: "Babayev", age:24, grade: 95},
+    
+//     {id:8, name: "Namiq", surname: "Gulahmedov", age:20, grade: 95},
+    
+//     {id:9, name: "Aygun", surname: "Kazimova", age:24, grade: 100}
+    
+//     ]
+
+
+//task2
+
+// let array = [] 
+
+//  for (let i = 0; i < students.length; i++){
+//     if ( i % 2 ==0)
+//     {
+//         array.push(students[i])
+//         console.log(i);
+//     }
+//  }
+// console.log(array);     //hazi
+
+
+
+
+//task3
+
+// for (let i = 0; i < students.length; i++) {
+//     if (students[i].id % 2)
+//     console.log(students[i].name);
+// }         //hazir
+
+
+//task4
+// let age = []
+
+// for (let i = 0; i < students.length; i++){
+//     if (students[i].age > 20){
+//         age.push(students[i]);
+//         console.log(students[i]);    
+//     }
+// }          //hazir
+
+//task5
+
+// let idGrade = []
+
+// for (let i = 0; i < students.length; i++){
+//     if(students[i].id % 2 ==0 && students[i].grade > 90)
+//     {
+//         idGrade.push(students[i]);
+//         console.log(students[i]);
+//     }
+// }        //hazir
+
+
+//task6
+
+// let grade = []
+
+// for (let i = 0; i < students.length; i++){
+//     if(students[i].grade === 100 || students[i].grade === 95){
+//         grade.push(students[i]);
+//         console.log(students[i]);
+//     }
+// }        //hazir
+
